7d0c5a36be6c0e6dd768691cbfad6d03785ee6d6
Harold Perry <harold@Harolds-MacBook-Pro.local>
2017-02-06 13:38:24 -0500

    Async File reads

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 // index.js
-const _ = require('lodash')
+// const _ = require('lodash')
 
 //require('./app/index')
 
-_.assign({'a': 1}, {'b': 2}, {'c': 3});
+// _.assign({'a': 1}, {'b': 2}, {'c': 3});
+
+const fs = require('fs')
+
+let content
+// try {
+//   content = fs.readFileSync('file.md', 'utf-8')
+//   console.log('File read ended.')
+// } catch (ex) {
+//   console.log(ex);
+// }
+//
+// console.log(content)
+
+console.log('start reading a file...')
+
+fs.readFile('file.md', 'utf-8', function(err, content) {
+  if (err) {
+    console.log('error happened during reading the file')
+
+    return console.log(err)
+
+  } else {
+    console.log('File read ended.')
+  }
+  console.log(content)
+})
+
+console.log('end of the file')
