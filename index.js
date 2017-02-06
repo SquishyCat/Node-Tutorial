36240b8c4d06a6a662d20c2d11489b659271fee9
Harold Perry <harold@squishycat.com>
2017-02-06 14:18:42 -0500

    Added express and express-handlebars for HTML rendering

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 // index.js
-// const _ = require('lodash')
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
 
-//require('./app/index')
+const app = express()
 
-// _.assign({'a': 1}, {'b': 2}, {'c': 3});
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
 
-const fs = require('fs')
-
-let content
-// try {
-//   content = fs.readFileSync('file.md', 'utf-8')
-//   console.log('File read ended.')
-// } catch (ex) {
-//   console.log(ex);
-// }
-//
-// console.log(content)
-
-console.log('start reading a file...')
-
-fs.readFile('file.md', 'utf-8', function(err, content) {
-  if (err) {
-    console.log('error happened during reading the file')
-
-    return console.log(err)
-
-  } else {
-    console.log('File read ended.')
-  }
-  console.log(content)
+app.use((request, response, next) => {
+  request.chance = 0
+  next()
 })
 
-console.log('end of the file')
+app.use((request, response, next) => {
+  request.chance = 1
+  next()
+})
+
+
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'John',
+    chance: request.chance
+  })
+})
+
+app.use((err, request, response, next) => {
+  console.log(err)
+  response.status(500).send('Something broke!')
+})
+
+app.listen(3000)
